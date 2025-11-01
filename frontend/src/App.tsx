8349ff0f6b36e9caa707c2aa8cba4815b90feec2
appletreecy <appletreecy@gmail.com>
2025-11-01 16:18:32 +1100

    completed the tailwind setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export default function App() {
+    return (
+        <div className="min-h-screen grid place-items-center">
+            <h1 className="text-3xl font-bold">Tailwind v4 OK ✅</h1>
+        </div>
+    );
+}
+
